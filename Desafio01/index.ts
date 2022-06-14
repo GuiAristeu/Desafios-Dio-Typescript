79944260f90b4dfa4e2f501d9f20b569282df43f
Guilherme Aristeu <guilhermearisteu@gmail.com>
2022-06-14 15:45:18 -0300

    desafio01 completo

---
+++ Desafio01/index.ts
@@ -0,0 +1,10 @@
+/* Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+ let employee = {};
+employee.code = 10;
+employee.name = "John"; */
+
+const employee = {
+    code: 10,
+    name: "John"
+};
